1d5b32c5903b17a671708f26eaffd118092f2ab4
Igor Cheremukhin <igor-cheremukhin@outlook.com>
2021-04-27 11:51:13 +0300

    fetch

--- src/components/posts/posts.tsx
+++ src/components/posts/posts.tsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const Posts = () => <div>Posts</div>;
+const apiUrl = 'https://gorest.co.in/public-api/posts';
+
+const Posts = () => {
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((resp) => setApiData(resp.data))
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log('apiData: ', apiData);
+  }, [apiData]);
+
+  return <div>Posts</div>;
+};
 
 export default Posts;
