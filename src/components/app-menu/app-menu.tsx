9bf79b9bc9cfbd5bc7876b77eb7149e9b732e523
Igor Cheremukhin <igor-cheremukhin@outlook.com>
2021-04-27 09:37:48 +0300

    Router

--- src/components/app-menu/app-menu.tsx
+++ src/components/app-menu/app-menu.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 
-const AppMenu = () => <div>AppMenu</div>;
+import Main from '../main/main';
+import Posts from '../posts/posts';
+
+const AppMenu = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={Main} />
+      <Route path="/posts/" component={Posts} />
+      <Redirect to="/" />
+    </Switch>
+  </Router>
+);
 
 export default AppMenu;
